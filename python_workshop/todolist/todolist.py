7514182ab666866a99bcde5330b05dccabacbcee
chjcmy <57761228+chjcmy@users.noreply.github.com>
2021-01-11 16:51:44 +0900

    파이썬

--- python_workshop/todolist/todolist.py
+++ python_workshop/todolist/todolist.py
@@ -1,4 +1,4 @@
-todolsits = []
+todolists = []
 index = 1
 
 while True:
@@ -6,7 +6,8 @@ while True:
     print("1. 리스트 추가")
     print("2. 리스트 목록 보기")
     print("3. 리스트 정보수정")
-    print("4.리스트 편집")
+    print("4. 리스트  삭제")
+    print("0. 종료")
     menu = input("메뉴를 선택하세요")
     if menu == "1":
         name = input("리스트 이름 : ")
@@ -14,17 +15,17 @@ while True:
         weather = input("날씨 : ")
         feel = input("느낀점 : ")
         setTodolist = {"id" : index, "name":name, "age":age, "weather":weather,"feel":feel}
-        todolsits.append(setTodolist)
+        todolists.append(setTodolist)
         print(setTodolist)
         index += 1
-    elif menu == "2" :
-        print(todolsits)
+    elif menu == "2":
+        print(todolists)
     elif menu == "3":
         id = input("수정할 리스트 번호를 입력 하시오")
-        while id.isdecimal() :
+        while not id.isdecimal():
             print("번호를 입력하시오")
             id = input("수정할 리스트 번호를 입력하시오")
-        for setTodolist in todolsits :
+        for setTodolist in todolists:
             if setTodolist["id"] == id:
                 modified = input("수정 할 메뉴를 선택하시오")
                 if modified in setTodolist.keys() :
@@ -33,12 +34,12 @@ while True:
                     print("리스트의 {0}번 째 {1} 부분이 {2}로 수정 되었습니다").format(setTodolist["id"],modified,text)
     elif menu == "4" :
         id = input("삭제할 리스트 번호를 입력하시오")
-        while id.isdecimal() :
+        while not id.isdecimal() :
             print("번호를 입려하세요")
             id = input("수정할 리스트 번호를 재입력하세요")
-        for setTodolist in todolsits :
+        for setTodolist in todolists :
             if setTodolist["id"] == id:
-                todolsits.remove(setTodolist)
+                todolists.remove(setTodolist)
                 print("{0}번 리스트 항복이 삭제 되었습니다".format(setTodolist["id"]))
 
     elif menu == "0":
